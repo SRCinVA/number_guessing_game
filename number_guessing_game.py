aa96758517ffcb0a2d0c054003c9fb18e952513b
SRCinVA <scitarella@hotmail.com>
2021-09-16 21:10:37 -0400

    first commit

---
+++ number_guessing_game.py
@@ -0,0 +1,7 @@
+import random
+
+r = random.randrange(-1, 10) # prints a random number with range parameters (exclusive of last number)
+                                # (last number alone is the stop)
+print(r)
+
+s = random.randint(-1, 10) # the same as above, but this time it goes to 11 : )
