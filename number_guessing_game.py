3f648a8a05bd523bf769388f52f1c5ef617bcf09
SRCinVA <scitarella@hotmail.com>
2021-10-17 20:07:57 -0400

    More progress

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -19,24 +19,20 @@ else:
     quit()
 
 random_number = random.randint(0,top_of_range) # the same as above, but this time it goes to 11 : )
-print(random_number)
 
 while True:
     user_guess = input("Make a guess: ")
-    input != random_number
 
     if user_guess.isdigit():
-    user_guess = int(user_guess)
+        user_guess = int(user_guess)
 
-    if user_guess <= 0:
-        print("Please type out a number larger than 0 next time.")
-        quit()
     else:
         print("Need an actual number, my friend.")
-        quit()
+        continue # unlike "break," "continue" brings us back to the top of the loop
+                 # nothing will run after "continue" if this else statement is invoked
+                 # if it isn't invoked, the while will unfold below.
 
-
-
-
-    print("Sorry, dude ...")
-    break 
+    if user_guess == random_number:
+        print("Correct!")
+    else:
+        print("Incorrect!")
